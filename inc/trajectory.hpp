30f95c2e7a569b1ea1b27aab7ac358a88c68aada
Murphy Han <11711416@mail.sustech.edu.cn>
2019-03-22 09:31:34 +0800

    add trajectory header file

--- inc/trajectory.hpp
+++ inc/trajectory.hpp
@@ -3,20 +3,38 @@
 
 #include "point.hpp"
 #include "line.hpp"
+#include <vector>
 
-template<typename T>
-class Trajectory{
+template <typename T>
+class Trajectory {
+     
+    public:
+        Trajectory();
+
+        void push(const T& rhs){
+            trajectory.push_back(rhs);
+        }
+
+        void insert(const T& rhs,std::vector<T>::iterator position){
+            trajectory.insert(position,rhs);
+        }
+
+        void remove(std::vector<T>::iterator rhs){
+            trajectory.erase(rhs);
+        }
+
+        std::size_t size(){
+            return trajectory.size();
+        }
+
+        T operator[](std::size_t position){
+            return trajectory[position];
+        }
+    private:
+        std::vector<T> trajectory;  
+   
 
 };
 
-template <>
-class Trajectory<Point> {
-
-};
-
-template <>
-class Trajectory<Line> {
-    
-};
 
 #endif
