b44ee6422d7a4ea7f8f5816124117a7c41bf5c54
Murphy Han <11711416@mail.sustech.edu.cn>
2019-03-24 10:05:25 +0800

    add shell script

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Trajectory-Compression-Algorithm
+### Overview
+
 A research project on trajectory compression
+
+This repository will include the following algorithm.
+
+
+
+
+
+#### Test 
+
+```
+chmod 777 taxi_dataset.sh
+./taxi_dataset.sh
+
+```
+
--- inc/point.hpp
+++ inc/point.hpp
@@ -1,16 +1,19 @@
 #ifndef POINT_HPP
 #define POINT_HPP
-// t is time stamp
-// x is longitude
-// y is latitude 
+#include <cmath>
+// x,y is 2d coordinate calculated by Mercator Projection
+const double RADIUS = 6371393;
 
 class Point{
     public:
-        double x,y,t;
-    
-    Point(double x_,double y_,double t_)
-        :x{x_},y{y_},t{t_}{
+        double longitude,latitude,t;
+        double x,y;
+    Point(double longitude_,double latitude_,double t_)
+        :longitude{longitude_},latitude{latitude_},t{t_}{
 
+            x = RADIUS * (longitude / 360.0) * 2.0 * acos(-1.0);
+            double theta = acos(-1.0) / 4.0 + (latitude / 2.0) / 360.0 * 2.0 * acos(-1.0);          
+            y = RADIUS * log(tan(theta));
     }
 };
 
