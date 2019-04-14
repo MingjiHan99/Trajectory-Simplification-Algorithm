40195ffdb36d3a79be3da0a81b12a95402410739
Murphy Han <11711416@mail.sustech.edu.cn>
2019-04-14 20:14:07 +0800

    Update clean_taxi.py

--- script/clean_taxi.py
+++ script/clean_taxi.py
@@ -3,17 +3,17 @@ import os
 directory = '../dataset/taxi/'
 
 n = 0
-for n in range(0,101):
+for n in range(0,1):
     file_name = 'A000' + str(n)
     path = os.path.join(directory,file_name)
 
     f = open(path,'r')
-    output = open(os.path.join('../dataset/taxi_clean/',file_name),'w')
+    output = open(os.path.join('../dataset/taxi_map/',file_name),'w')
 
     for line in f.readlines():
             row = line.split(';')
             print(row)
-            output.write(row[1]+' '+row[2]+' '+row[3]+'\n')
+            output.write(row[2]+','+row[3]+','+'Beijing' + ',' + '#FF0000'+ '\n')
 
     f.close()
     output.close()
