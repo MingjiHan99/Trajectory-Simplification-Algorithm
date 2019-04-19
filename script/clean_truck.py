7dbc8ecadb117d2f9839426f02e673c7b2f12715
Murphy Han <11711416@mail.sustech.edu.cn>
2019-04-19 20:02:52 +0800

    add script

--- script/clean_truck.py
+++ script/clean_truck.py
@@ -1,14 +1,14 @@
 import os
 
-directory = '../dataset/taxi/'
+directory = '../dataset/truck/'
 
 n = 0
 for n in range(0,100):
-    file_name = 'A000' + str(n)
+    file_name = 'TK000' + str(n)
     path = os.path.join(directory,file_name)
 
     f = open(path,'r')
-    output = open(os.path.join('../dataset/taxi_map/',file_name),'w')
+    output = open(os.path.join('../dataset/truck_map/',file_name),'w')
     count = 0
     for line in f.readlines():
             row = line.split(';')
---
+++ src/main2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+#include <string>
+#include "../inc/trajectory.hpp"
+#include "../inc/operb.hpp"
+#include "../inc/algorithm.hpp"
+#include "../inc/dp.hpp"
+#include "../inc/fbqs.hpp"
+#pragma comment(linker, "/STACK:1024000000,1024000000") 
+
+std::string algorithm_type[4] = {"dp","operb","operba","fbqs"};
+int main(int argc,char *argv[]){
+    
+    if(argc < 4){
+        return 0;
+    }
+
+    double error_bound = std::stod(argv[1]);
+    int size = std::stoi(argv[2]);
+    int total_point = 0;
+    Algorithm* pta = nullptr;
+  
+    if(argv[3] == algorithm_type[0] ){
+        pta = new DP{error_bound};
+     
+    }
+    else if(argv[3] == algorithm_type[1]){
+         pta = new OPERB{error_bound};
+    }
+    else if(argv[3] == algorithm_type[2]){
+        pta = new OPERBA{error_bound};
+    }
+    else if(argv[3] == algorithm_type[3]){
+        pta = new FBQS{error_bound};
+    }
+    
+
+    double tx,ty,tt;
+    double averge_rate,temp_rate,average_second;
+    double total_second = 0.0;
+
+    for(int i = 0 ; i < size; i++){
+        Trajectory<Point>* traj = new Trajectory<Point>;
+
+        std::string file_name = "../dataset/taxi_map/A000" + std::to_string(i);
+        freopen(file_name.c_str(),"r",stdin);
+        int count = 0;
+        while(scanf("%lf %lf %lf",&tt,&ty,&tx) == 3){
+            std::cout <<"Yes" << std::endl;
+            traj->push(Point{tx,ty,tt});
+        }
+        total_point += traj->size();
+
+        std::cout << "Running on No." << i << " trajectory..." << std::endl;
+        std::cout << "Trajectory size:" << traj->size() << std::endl;
+        
+        double start_time = clock();
+        auto result = pta->compress(traj);
+        double end_time = clock();
+        total_second += (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+        
+        temp_rate = (double) (traj->size() - result->size() - 1)/ traj->size();
+        averge_rate += temp_rate;
+
+        std::cout << "Compressed trajectory size:" << result->size() + 1 << std::endl;
+        std::cout << "Compression rate:" << temp_rate << std::endl;
+        
+        delete traj;
+        fclose(stdin);
+    
+    }
+
+    
+    averge_rate /= size;
+    average_second /= size;
+    freopen("result.txt","w",stdout);
+    std::cout << "Total points:" << total_point << std::endl;
+    std::cout << "Error Bound: " << error_bound << "m" << std::endl;
+    std::cout << "Average Compression Ratio: " << averge_rate * 100 << "\%" << std::endl;
+    std::cout << "Average Running time " << (double) average_second << "s"<< std::endl;
+
+    fclose(stdout);
+    
+    return 0;
+}
